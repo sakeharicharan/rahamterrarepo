183484513f3be78015367ea0cec3928f81acd1ed
sakeharicharan <Sakeharicharan0123@gmail.com>
2025-10-25 13:00:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "three" {
   count         = 2
-  ami           = ""ami-00af95fa354fdb788
+  ami           = "ami-00af95fa354fdb788"
   instance_type = "t3.micro"
 
   tags = {
